02b40ab931501832fd0a29869de0a5c51018c255
Cristian <cristian10damian@gmail.com>
2023-12-18 19:54:41 -0500

    Import_Plantilla_Alert

--- autoload.php
+++ autoload.php
@@ -1,5 +1,4 @@
 <?php
-echo __DIR__;
     spl_autoload_register(function($clase){
         $archivo = __DIR__."/".$clase.".php";   /* Obtiene el directorio actual*/
         $archivo = str_replace("\\", "/", $archivo);
